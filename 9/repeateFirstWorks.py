8a7e56ab4fcca50730702b5206659cb5d56b3f66
Denis Rakov <denivityaz@gmail.com>
2023-11-13 23:54:28 +0300

    changes in 9th wotk

--- 9/repeateFirstWorks.py
+++ 9/repeateFirstWorks.py
@@ -1,4 +1,5 @@
 from termcolor import colored       # Косметичка
+from datetime import datetime
 
 class UserInput:        # Методы для пользовательского ввода: строка, int и getEnter
 
@@ -14,7 +15,8 @@ class UserInput:        # Методы для пользовательского
                 value = int(input(prompt))
                 return value
             except ValueError:
-                print("Error: Enter a valid integer!")
+                print(f"{colored('Error:', 'red')} Enter a valid integer!")
+                
 
     @staticmethod
     def getInt_list(prompt="Enter numbers separated by a space: "):
@@ -26,12 +28,12 @@ class UserInput:        # Методы для пользовательского
                 if part.isdigit():      #если циферка пробуем в инт закинуть
                     result.append(int(part))
                 else:                               #будет чтото ошибочное не заново, просто пропускаем и указываем
-                    print(f"Incorrect input: '{part}' not a number.")
+                    print(f"{colored('Incorrect input:', 'red')} '{part}' not a number.")
 
             if result:      #если все неправильно, ну извините
                 return result
             else:
-                print("Error: Enter a valid values!")
+                print(f"{colored('Error:', 'red')} Enter a valid values!")
 
     @staticmethod
     def getEnter():
@@ -47,7 +49,30 @@ class MyFunc:
     
     def NOK(self, x, y, z):
         return x * y // z
+    
+    def simple_brute_force(self, num):     #метод перебора, другие способы не очень понимаю
+    
+        def isSimple(num):
+            if num < 2:
+                return False
+            # Перебираем числа от 2 до корня из num
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    return False
+            # Если не нашли делителей, то num простое
+            return True
+
+        # Все в список
+        primes = [i for i in range(2, num + 1) if isSimple(i)]
+        return primes
         
+        # список делителей
+    def dividers(self, num):
+        start_time = datetime.now ()
+        result = [i for i in range (1, num // 2 + 1) if num % i == 0] + [num]   # Генерирует все числа от 1 до num\\2
+        end_time = datetime.now ()
+        print ('time: ', end_time - start_time)
+        return result
         
 
         
@@ -77,11 +102,16 @@ class Main:
             user.getEnter()
 
         elif select == "2": 
-            
+            num = user.getInt()
+            result = func.simple_brute_force(num)
+            print(f"Простые числа от 1 до {num}:", *result)
             user.getEnter()
 
         elif select == "3": 
-
+            
+            num = user.getInt()
+            result = func.dividers(num)
+            print(*result)
             user.getEnter()
 
         elif select == "0": 
